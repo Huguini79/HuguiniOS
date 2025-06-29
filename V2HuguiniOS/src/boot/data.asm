a53fb94257d14ad48e70b012d7b2d00d94eb30dd
Huguini79 <166934655+Huguini79@users.noreply.github.com>
2025-06-29 13:42:04 +0100

    Delete V2HuguiniOS directory

--- V2HuguiniOS/src/boot/data.asm
+++
@@ -1,8 +0,0 @@
-imprimir db 'BIENVENIDO A HuguiniOS', 0x0D, 0x0A
-         db 'Creador: Huguini', 0x0D, 0x0A
-         db 'Hecho en ensamblador', 0x0D, 0x0A, 0x0D
-         db '............', 0x0D, 0x0A
-         db 'HuguiniOS', 0x0D, 0x0A
-         db '............', 0x0D, 0x0A, 0x0D
-         db 'Creado teniendo 12 de edad', 0x0D, 0x0A
-         db '................', 0
